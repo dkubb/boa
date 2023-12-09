7f68cc42292ccece01b507d2c3b850eac0f53621
Dan Kubb <github@dan.kubb.ca>
2023-12-09 10:54:17 -0800

    Fix sorbet config to check the spec subdirectory
    * Fix spec/doctest_helper.rb to pass strict type checks.

--- spec/doctest_helper.rb
+++ spec/doctest_helper.rb
@@ -1,4 +1,4 @@
-# typed: strong
+# typed: strict
 # frozen_string_literal: true
 
 require 'simplecov'
@@ -8,58 +8,41 @@ require 'boa'
 
 extend T::Sig
 
-class Person < T::Struct
-  include Boa
+class Equality
+  extend T::Sig
+  include Boa::Equality
 
-  prop :name,  String, default: 'Dan Kubb', length: 1..50
-  prop :email, String
-  prop :admin, T::Boolean, default: false
-end
+protected
 
-sig { returns(Boa) }
-def instance
-  @instance ||= T.assert_type!(Person.new(email: '', admin: true), T.nilable(Person))
+  sig { override.returns(T::Hash[Symbol, Object]) }
+  def object_state
+    { object_id: }
+  end
 end
 
 sig { returns(Boa::Type) }
 def type
-  @type ||= T.assert_type!(Class.new(Boa::Type).new(:first_name, default: 'Jon'), T.nilable(Boa::Type))
+  @type ||= T.let(Class.new(Boa::Type).new(:first_name, default: 'Jon'), T.nilable(Boa::Type))
 end
 
-sig { returns(T::Class[Boa]) }
-def descendant
-  @descendant ||= T.assert_type!(Class.new(T::Struct) { include Boa }, T.nilable(T::Class[Boa]))
-end
-
-sig { returns(T.class_of(Boa::Equality)) }
+sig { returns(T::Class[Boa::Equality]) }
 def equality_class
-  @equality_class ||=
-    Class.new do
-      extend T::Sig
-      include Boa::Equality
-
-    protected
-
-      sig { override.returns(T::Hash[Symbol, Object]) }
-      def object_state
-        { object_id: }
-      end
-    end
+  @equality_class ||= T.let(Equality, T.nilable(T::Class[Boa::Equality]))
 end
 
 sig { returns(Boa::Equality) }
 def equality_object
-  @equality_object ||= T.assert_type!(equality_class.new, T.nilable(Boa::Equality))
+  @equality_object ||= T.let(equality_class.new, T.nilable(Boa::Equality))
 end
 
 sig { returns(Object) }
 def other
-  @other ||= Object.new
+  @other ||= T.let(Object.new, T.nilable(Object))
 end
 
 YARD::Doctest.configure do |doctest|
   doctest.before('Boa::Equality') do
-    @other = T.assert_type!(equality_class.new, T.nilable(Boa::Equality))
+    @other = T.let(equality_class.new, T.nilable(Boa::Equality))
   end
 
   doctest.before('Boa::Type::Boolean') do
@@ -68,9 +51,5 @@ YARD::Doctest.configure do |doctest|
 
   doctest.before('Boa::Type::String') do
     @type = Boa::Type::String.new(:first_name)
-  end
-
-  doctest.before do
-    @instance = nil
   end
 end
