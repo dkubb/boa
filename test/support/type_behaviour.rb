c04b7350effd00da0a083af568b8dd5243a8cd1b
Dan Kubb <github@dan.kubb.ca>
2023-12-11 07:16:36 -0800

    Change Boa::Type class method tests to be in Support::TypeBehaviour

--- test/support/type_behaviour.rb
+++ test/support/type_behaviour.rb
@@ -3,7 +3,147 @@
 
 module Support
   module TypeBehaviour
+    extend T::Sig
     extend SharedSetup
+
+    sig { returns(T.class_of(Boa::Type)) }
+    def type_class
+      @type_class ||= Class.new(described_class)
+    end
+
+    sig { returns(Module) }
+    def class_type
+      @class_type ||= Class.new
+    end
+
+    sig do
+      params(klass: T.class_of(Object), method_name: Symbol, block: T.proc.params(descendant: T.class_of(Boa::Type)).void).void
+    end
+    def replace_method(klass, method_name, &block)
+      original_verbose = $VERBOSE
+      $VERBOSE         = nil
+
+      # Replace the singleton method without warnings
+      klass.define_singleton_method(method_name, &block)
+
+      $VERBOSE = original_verbose
+    end
+
+    shared_examples 'Boa::Type.[]' do
+      cover 'Boa::Type.[]'
+
+      subject { described_class[class_type] }
+
+      describe 'when class type is registered' do
+        sig { returns(Module) }
+        def class_type
+          String
+        end
+
+        it 'returns the expected type class' do
+          assert_same(Boa::Type::String, subject)
+        end
+      end
+
+      describe 'when class type is not registered' do
+        it 'returns the default type class' do
+          # assert there is no explict mapping for the class type
+          error =
+            assert_raises(ArgumentError) do
+              described_class[class_type]
+            end
+
+          assert_equal("type class for #{class_type} is unknown", error.message)
+        end
+      end
+    end
+
+    shared_examples 'Boa::Type.[]=' do
+      cover 'Boa::Type.[]='
+
+      subject { described_class[class_type] = type_class }
+
+      after do
+        # Remove the type class from the registry
+        described_class.send(:class_types).delete(class_type) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
+      end
+
+      it 'sets the class type' do
+        # assert there is no explict mapping for the class type
+        assert_raises(ArgumentError) do
+          described_class[class_type]
+        end
+
+        subject
+
+        assert_same(type_class, described_class[class_type])
+      end
+    end
+
+    shared_examples 'Boa::Type.class_type' do
+      cover 'Boa::Type.class_type'
+
+      subject { described_class.class_type(class_type) }
+
+      after do
+        # Remove the type class from the registry
+        described_class.send(:class_types).delete(class_type) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
+      end
+
+      it 'returns the type class' do
+        assert_same(described_class, subject)
+      end
+
+      it 'sets the class type' do
+        # assert there is no explict mapping for the class type
+        assert_raises(ArgumentError) do
+          described_class[class_type]
+        end
+
+        subject
+
+        assert_same(described_class, described_class[class_type])
+      end
+    end
+
+    shared_examples 'Boa::Type.inherited' do
+      cover 'Boa::Type.inherited'
+
+      subject { Class.new(described_class) }
+
+      it 'set the class types' do
+        assert_same(described_class.send(:class_types), subject.send(:class_types)) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
+      end
+
+      it 'calls super' do
+        described_class    = self.described_class
+        object_inherited   = Object.method(:inherited)
+        original_inherited = described_class.method(:inherited)
+        inherited          = []
+
+        # Override described_class.inherited
+        replace_method(described_class, :inherited) do |descendant|
+          inherited << described_class
+          original_inherited.call(descendant)
+        end
+
+        # Override Object.inherited
+        replace_method(Object, :inherited) do |descendant|
+          inherited << Object
+          object_inherited.call(descendant)
+        end
+
+        subject
+
+        assert_equal([described_class, Object], inherited)
+
+        # Restore Object.inherited
+        replace_method(Object, :inherited, &object_inherited)
+
+        # Restore described_class.inherited
+        replace_method(described_class, :inherited, &original_inherited)
+      end
+    end
 
     shared_examples 'Boa::Type.new' do
       cover 'Boa::Type.new'
--- test/unit/boa/type_test.rb
+++ test/unit/boa/type_test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 
 describe Boa::Type do
   extend T::Sig
+  include Support::TypeBehaviour
 
   sig { returns(T.class_of(Boa::Type)) }
   def described_class
@@ -12,151 +13,18 @@ describe Boa::Type do
   end
 
   describe '.[]' do
-    cover 'Boa::Type.[]'
-
-    subject { described_class[class_type] }
-
-    describe 'when class type is registered' do
-      sig { returns(Module) }
-      def class_type
-        String
-      end
-
-      it 'returns the expected type class' do
-        assert_same(Boa::Type::String, subject)
-      end
-    end
-
-    describe 'when class type is not registered' do
-      sig { returns(Module) }
-      def class_type
-        @class_type ||= Class.new
-      end
-
-      it 'returns the default type class' do
-        # assert there is no explict mapping for the class type
-        error =
-          assert_raises(ArgumentError) do
-            described_class[class_type]
-          end
-
-        assert_equal("type class for #{class_type} is unknown", error.message)
-      end
-    end
+    include_examples 'Boa::Type.[]'
   end
 
   describe '.[]=' do
-    cover 'Boa::Type.[]='
-
-    subject { described_class[class_type] = type_class }
-
-    sig { returns(Module) }
-    def class_type
-      @class_type ||= Class.new
-    end
-
-    sig { returns(T.class_of(Boa::Type)) }
-    def type_class
-      @type_class ||= Class.new(described_class)
-    end
-
-    after do
-      # Remove the type class from the registry
-      described_class.send(:class_types).delete(class_type) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
-    end
-
-    it 'sets the class type' do
-      # assert there is no explict mapping for the class type
-      assert_raises(ArgumentError) do
-        described_class[class_type]
-      end
-
-      subject
-
-      assert_same(type_class, described_class[class_type])
-    end
+    include_examples 'Boa::Type.[]='
   end
 
   describe '.class_type' do
-    cover 'Boa::Type.class_type'
-
-    subject { described_class.class_type(class_type) }
-
-    sig { returns(Module) }
-    def class_type
-      @class_type ||= Class.new
-    end
-
-    after do
-      # Remove the type class from the registry
-      described_class.send(:class_types).delete(class_type) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
-    end
-
-    it 'returns the type class' do
-      assert_same(described_class, subject)
-    end
-
-    it 'sets the class type' do
-      # assert there is no explict mapping for the class type
-      assert_raises(ArgumentError) do
-        described_class[class_type]
-      end
-
-      subject
-
-      assert_same(described_class, described_class[class_type])
-    end
+    include_examples 'Boa::Type.class_type'
   end
 
   describe '.inherited' do
-    cover 'Boa::Type.inherited'
-
-    subject { Class.new(described_class) }
-
-    sig do
-      params(klass: T.class_of(Object), method_name: Symbol, block: T.proc.params(descendant: T.class_of(Boa::Type)).void).void
-    end
-    def replace_method(klass, method_name, &block)
-      original_verbose = $VERBOSE
-      $VERBOSE         = nil
-
-      # Replace the singleton method without warnings
-      klass.define_singleton_method(method_name, &block)
-
-      $VERBOSE = original_verbose
-    end
-
-    it 'set the class types' do
-      assert_same(described_class.send(:class_types), subject.send(:class_types)) # rubocop:disable Style/DisableCopsWithinSourceCodeDirective,Style/Send
-    end
-
-    it 'calls super' do
-      superclass           = Boa::Type.superclass
-      superclass_inherited = superclass.method(:inherited)
-      original_inherited   = Boa::Type.method(:inherited)
-      inherited            = []
-
-      # Override Boa::Type.inherited
-      replace_method(Boa::Type, :inherited) do |descendant|
-        inherited << Boa::Type
-        original_inherited.call(descendant)
-      end
-
-      # Override superclass.inherited
-      replace_method(superclass, :inherited) do |descendant|
-        inherited << superclass
-        superclass_inherited.call(descendant)
-      end
-
-      subject
-
-      assert_equal([Boa::Type, superclass], inherited)
-
-      # Restore superclass.inherited
-      replace_method(superclass, :inherited, &superclass_inherited)
-
-      # Restore Boa::Type.inherited
-      replace_method(Boa::Type, :inherited, &original_inherited)
-    end
+    include_examples 'Boa::Type.inherited'
   end
 end
