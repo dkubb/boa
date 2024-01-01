9af02d21df44f195c82c1cb5adfd9b1419879517
Dan Kubb <github@dan.kubb.ca>
2024-01-01 12:00:02 -0800

    Fix test helper require order

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,8 @@ require 'minitest/autorun'
 require 'mutant/minitest/coverage'
 require 'sorbet-runtime'
 
-require_relative 'support/mutant_coverage'
 require_relative 'support/instance_methods_behaviour'
+require_relative 'support/mutant_coverage'
 require_relative 'support/type_behaviour'
 
 require 'boa'
